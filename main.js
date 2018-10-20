13d80da88e7b04f7c8ded9abc35ba64b714f1fd3
Gianluca Frongia <gianluca.frongia@gibmit.ch>
2018-10-20 13:44:10 +0200

    Layout almost done

--- main.js
+++ main.js
@@ -8,4 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	var instances = M.Materialbox.init(elems);
  });
 
- 
+ document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.fixed-action-btn');
+	var instances = M.FloatingActionButton.init(elems, {
+		direction: 'left',
+		hoverEnabled: false
+	});
+	console.log(elems);
+	
+	var instance = M.FloatingActionButton.getInstance(elems[0]);
+	instance.open();
+ });
+
